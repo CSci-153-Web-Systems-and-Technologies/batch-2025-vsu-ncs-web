f406f80984db9c3d58ab8f0d8745fe122a4df724
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-04 03:54:24 +0800

    Added styling and data handling for logged records page

--- app/(main-pages)/protected/faculty/logged-records/page.tsx
+++ app/(main-pages)/protected/faculty/logged-records/page.tsx
@@ -1,3 +1,49 @@
-export default function LoggedRecords() {
-  return <div>This is faculty logged records.</div>;
+import ReportCardList from "./_components/report-card-list";
+import { createClient } from "@/lib/supabase/server";
+import { transformReportForFaculty, safeMap } from "@/lib/transformers"; // <--- NEW UTILS
+import { ConductReportWithStudent } from "@/types";
+
+export default async function ReportsPage() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // 1. FETCH RAW DATA
+  const { data: rawData } = await supabase
+    .from("conduct_reports")
+    .select(
+      `
+      *,
+      student:student_profiles(*),
+      infraction_responses (
+        created_at,
+        admin:staff_profiles(first_name, last_name)
+      )
+    `
+    )
+    .eq("faculty_id", user?.id)
+    .order("created_at", { ascending: false });
+
+  // 2. TRANSFORM DATA
+  const records: ConductReportWithStudent[] = safeMap(
+    rawData,
+    transformReportForFaculty
+  );
+
+  return (
+    <div className="flex flex-col w-full p-8 gap-5">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-[#0A58A3] text-2xl">My Conduct Reports</h1>
+        <p className="text-[#6C757D]">
+          Complete record of all merits, demerits, and serious infractions
+          reports.
+        </p>
+      </div>
+      <div>
+        <ReportCardList data={records} />
+      </div>
+    </div>
+  );
 }
