9e172d1e676947d24ac5aff8754bc2ab49fe2e38
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-04 18:33:29 +0800

    fix: Removed unused imports and fetch in quick action card component

--- app/(main-pages)/protected/faculty/dashboard/_components/quick-action-card.tsx
+++ app/(main-pages)/protected/faculty/dashboard/_components/quick-action-card.tsx
@@ -7,7 +7,6 @@ import {
 } from "@/components/ui/card";
 import { RecordForm } from "./record-form";
 import { createClient } from "@/lib/supabase/server";
-import { StaffProfile } from "@/types";
 
 /*type QuickActionCardProps = {
   title: string;
@@ -18,26 +17,12 @@ import { StaffProfile } from "@/types";
 
 export default async function QuickActionCard() {
   const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
 
-  // 1. Fetch Profile
-  const { data: profileData } = await supabase
-    .from("staff_profiles")
-    .select("*")
-    .eq("id", user?.id)
-    .single();
-  const profile = profileData as StaffProfile;
-
-  // 1. Fetch minimal student data for the dropdown
-  // We only need names and IDs to make the search work
   const { data: studentsRaw } = await supabase
     .from("student_profiles")
     .select("id, student_id, first_name, last_name")
     .order("last_name", { ascending: true });
 
-  // 2. Map to the shape our form expects
   const studentOptions = (studentsRaw || []).map((s) => ({
     id: s.id,
     student_id: s.student_id || "No ID",
@@ -50,7 +35,7 @@ export default async function QuickActionCard() {
         <CardDescription>Record student conduct.</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col">
-        <RecordForm students={studentOptions} faculty={profile} />
+        <RecordForm students={studentOptions} />
       </CardContent>
     </Card>
   );
