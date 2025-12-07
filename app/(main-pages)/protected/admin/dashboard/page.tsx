d671b837f32ee0ed11601d6b6f5b6b595af0f0b8
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-08 07:43:20 +0800

    fix: Fixed spacing in dashboard charts

--- app/(main-pages)/protected/admin/dashboard/page.tsx
+++ app/(main-pages)/protected/admin/dashboard/page.tsx
@@ -101,7 +101,7 @@ export default async function AdminDashboard() {
           description="In need of review"
         />
       </div>
-      <div className="space-y-6 p-6">
+      <div className="flex flex-col gap-6">
         <h1 className="text-3xl font-bold tracking-tight">
           Dashboard Overview
         </h1>
