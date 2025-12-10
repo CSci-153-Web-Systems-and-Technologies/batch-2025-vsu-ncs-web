7aba37831a6681c3fee4ffeae3ca7d6801117c32
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-11 07:32:11 +0800

    Updated nodemailer email and username to use dedicated email for project

--- lib/email-transporter.ts
+++ lib/email-transporter.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "jamirandrade4270@gmail.com",
+    user: "vsu.ncs@gmail.com",
     pass: process.env.GMAIL_APP_PASSWORD,
   },
 });
@@ -11,7 +11,7 @@ export const transporter = nodemailer.createTransport({
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
     const info = await transporter.sendMail({
-      from: '"VSU NCS" <jamirandrade4270@gmail.com>',
+      from: '"VSU NCS" <vsu.ncs@gmail.com>',
       to: to,
       subject: subject,
       html: html,
