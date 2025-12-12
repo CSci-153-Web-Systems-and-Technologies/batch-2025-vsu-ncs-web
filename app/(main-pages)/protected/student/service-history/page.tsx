03ebaf4175bc71699d05265b3326b71416eb7bf8
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-12-12 16:40:29 +0800

    Integrated components to service history page

--- app/(main-pages)/protected/student/service-history/page.tsx
+++ app/(main-pages)/protected/student/service-history/page.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
-import { transformServiceLog, safeMap } from "@/lib/data"; // Or your utils path
+import { transformServiceLogWithReporter, safeMap } from "@/lib/data"; // Or your utils path
 import { ServiceLogWithReporter } from "@/types";
 import ServiceLogList from "./_components/service-log-list";
 import { AlertTriangle } from "lucide-react";
@@ -11,48 +11,34 @@ export default async function ServiceHistory() {
   } = await supabase.auth.getUser();
 
   const { data: rawData } = await supabase
-    .from("conduct_reports")
+    .from("service_logs")
     .select(
       `
-          *,
-          student:student_profiles(*), 
-          reporter:staff_profiles!faculty_id (first_name, last_name, title),
-          
-          infraction_responses (
-            id,
-            created_at, 
-            final_sanction_days,
-            final_sanction_other,
-            notes,
-            admin:staff_profiles (first_name, last_name)
-          )
-        `
+      *,
+      reporter:staff_profiles!faculty_id (first_name, last_name, title),
+    `
     )
-    .eq("is_serious_infraction", true)
     .eq("student_id", user?.id)
     .order("created_at", { ascending: false });
 
-  // 2. TRANSFORM
-  const seriousReports: SeriousInfractionTicket[] = safeMap(
+  const serviceReports: ServiceLogWithReporter[] = safeMap(
     rawData,
-    transformSeriousTicket
+    transformServiceLogWithReporter
   );
 
   return (
     <div className="flex flex-col w-full p-8 gap-8 max-w-5xl mx-auto">
-      {/* Header Section */}
       <div className="flex flex-col gap-2">
-        <div className="flex items-center gap-2 text-red-600">
+        <div className="flex items-center gap-2 text-slate-600">
           <AlertTriangle className="w-8 h-8" />
-          <h1 className="text-2xl font-bold">Serious Infractions</h1>
+          <h1 className="text-2xl font-bold">Service History</h1>
         </div>
         <p className="text-muted-foreground">
           These are major violations that require administrative review.
         </p>
       </div>
 
-      {/* The List Component */}
-      <SeriousInfractionList data={seriousReports} />
+      <ServiceLogList data={serviceReports} />
     </div>
   );
 }
